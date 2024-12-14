cf7ca05df41ddfb59225f4563fc3694ba4208e1e
K-P-D-0 <platon1981@icloud.com>
2024-12-14 15:46:12 +0500

    Попытка изменения

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,6 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<SportsObject> sportsObjects = CSVParser.Parsing("Объекты спорта.csv");
+        System.out.print("Hello world!!!");
     }
 }
